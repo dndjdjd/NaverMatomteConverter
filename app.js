4636867edd4ae53828200c094cc542c14e7d54d9
bisque33 <bisque33@gmail.com>
2014-03-12 00:56:41 +0900

    不要なプログラムの削除

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 
-// process.env.TZ = "Asia/Tokyo";
-
-// require('newrelic');
-// require('./lib/cron');
-
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var ect = require('ect');
 var fs = require('fs');
 var request = require('request');
-// var httpServer = require('http').createServer();
-
-// var db = require('./lib/db');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
 app.engine('ect', ect({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
 app.set('view engine', 'ect');
 app.use(express.favicon());
@@ -27,7 +18,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+// app.use(express.cookieParser('your secret here'));
 //app.use(express.session());
 
 // // sessionStoreを設定しないと、Node.jsのメモリストアーにセッションが保存される
@@ -60,57 +51,17 @@ var httpServer = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-// httpがlistenを開始してからsocket.ioのlistenを行う。
-// 順番が逆になると初回アクセスにsocket.io.jsがnot foundになる。
+// ソケット通信開始 ==========================
 var socketIo = require('socket.io').listen(httpServer);
 
-// ソケット通信開始 ==========================
 socketIo.sockets.on('connection', function(socket){
 	var Scraping = require('./lib/scraping').Scraping;
 	var scraping = new Scraping();
 
-	// シリーズリスト取得要求
+	// JSON取得要求
 	socket.on('get', function(url, callback){
 		scraping.getLocation(url, function(location){
 			callback(location);
-		});
-	});
-
-	// 曲リスト取得要求
-	socket.on('musics', function(query, callback){
-		db.Musics.find(query[0]).sort(query[1]).exec(function(error, result){
-			if(error) return console.log(error);
-			callback(result);
-		});
-	});
-
-	// movieデータ取得イベント
-	socket.on('movies', function(data){
-		// music_idからtitleを検索
-		db.Musics.find({_id: data}, function(error, results){
-			if (error) return "music not found";
-			if (results.length === 0) return "music not found";
-
-			var title = results[0].title;
-			// Memo: parameter
-			// category
-			// orderby
-
-			// http://gdata.youtube.com/feeds/api/videos?q=beatmania&max-results=10&alt=json
-			var requestUrl = 'http://gdata.youtube.com/feeds/api/videos?' +
-								'q=beatmania+' + title +'&max-results=20&alt=json';
-			request({url: requestUrl}, function(error, response, body) {
-				if(!error && response.statusCode == 200){
-					socket.emit('movies', body, function(data){
-						console.log(data);
-					});
-				}
-				else{
-					return "youtube api error";
-				}
-
-			});
 		});
 	});
 });
